d442204cc56fda3271992ba526ec1719a4a58acd
Edwin Hagen <ehagen@tedon.com>
2021-11-05 14:19:19 +0100

    Fixed loading Cachefile DateTime on all PowerShell versions and Platforms

--- src/Scriptbook/Scriptbook.psm1
+++ src/Scriptbook/Scriptbook.psm1
@@ -175,26 +175,29 @@ else
         # determine if we need to load the modules from repository again
         # now we check once a day the repository feed if new version are available 
         # to speed up the start-time of our workbooks
+        $md = $null
         $moduleCache = Get-Content -Path $cacheTimeFile -Raw | ConvertFrom-Json
-        try
+        if (Test-PSProperty -Object $moduleCache -Name 'Time') 
         {
-            if ($moduleCache.Time.Value -is [string])
-            {            
-                $md = [System.DateTime]::Parse($moduleCache.Time.Value).Date
+            if (Test-PSProperty -Object $moduleCache.Time -Name 'Value')
+            {
+                if ($moduleCache.Time.Value -is [string])
+                {            
+                    $md = [System.DateTime]::Parse($moduleCache.Time.Value).Date
+                }
+                elseif ($moduleCache.Time.Value -is [DateTime])
+                {
+                    $md = $moduleCache.Time.Value.Date
+                }
             }
             else
             {
-                $md = $moduleCache.Time.Value.Date
+                $md = [System.DateTime]::Parse($moduleCache.Time).Date
             }
-            if ($md -eq ((Get-Date).Date))
+            if ($md -and ($md -eq ((Get-Date).Date)))
             {
                 $checkModules = $false
             }
-        }
-        catch
-        {
-            # issues with reading dt values on different platforms
-            # reinitialize cache file
         }
     }
 }
